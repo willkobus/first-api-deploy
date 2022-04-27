8ccfc9386fecaafd23391594f782d79a672f57fd
wkkobus <willkobus@gmail.com>
2022-04-27 16:53:27 -0400

    port change 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const server = express(); // this server is deaf AF. Can't hear ANYTHING.
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log("Server Listening...");
