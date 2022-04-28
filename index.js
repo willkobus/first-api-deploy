9f1d20910718ffdd8262dc2d22058944f8a4024e
wkkobus <willkobus@gmail.com>
2022-04-28 11:02:01 -0400

    added / path

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ server.listen(PORT, () => {
   console.log("Server Listening...");
 });
 
+server.get("/", (req, res) => {
+  res.send("<h1>This is the landing page</h1>");
+});
+
 server.get("/will", (req, res) => {
   res.send("<h1>Hi Will Is the coolest</h1>");
 });
